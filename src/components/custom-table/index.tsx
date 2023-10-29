c6cf008a4a465660f4c43143df6520807f429611
serifcolakel <serifcolakel0@gmail.com>
2023-10-29 23:34:22 +0300

    INFO (serif) : custom-table component added.

---
+++ src/components/custom-table/index.tsx
@@ -0,0 +1,129 @@
+import { MemoryStickIcon } from 'lucide-react';
+import { flexRender } from '@tanstack/react-table';
+
+import { Button } from '@components/ui/button';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@components/ui/table';
+
+import useTable from './hooks/useTable';
+import { CustomTableProps } from './types';
+
+function CustomTable<T extends Record<string, unknown>>({
+  columns,
+  data,
+  setData,
+  options,
+  pageSize,
+}: CustomTableProps<T>) {
+  const { table, handleExport } = useTable<T>({
+    columns,
+    data,
+    setData,
+    options,
+    pageSize,
+  });
+
+  return (
+    <div className="w-full p-4 border rounded-lg">
+      <Button onClick={() => handleExport('excel')} size="sm" variant="outline">
+        Export
+      </Button>
+      <Button onClick={() => handleExport('excel')} size="sm" variant="outline">
+        Export
+      </Button>
+      <div className="border rounded-md">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead
+                      className="border-r last:border-r-0"
+                      key={header.id}
+                    >
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  data-state={row.getIsSelected() && 'selected'}
+                  key={row.id}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  className="h-24 text-center"
+                  colSpan={columns.length}
+                >
+                  <div className="p-4 space-y-4 border rounded-lg bg-gray-50">
+                    <MemoryStickIcon className="inline-block w-8 h-8 text-gray-500" />
+                    <div className="text-3xl font-medium text-gray-500">
+                      No data available.
+                    </div>
+                    <div className="text-sm text-gray-400">
+                      There is no data to display.
+                    </div>
+                  </div>
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <div className="flex items-center justify-center py-4 space-x-2">
+        <div className="flex-1 text-sm text-muted-foreground">
+          {table.getFilteredSelectedRowModel().rows.length} of{' '}
+          {table.getFilteredRowModel().rows.length} row(s) selected.
+        </div>
+        <div className="space-x-2">
+          <Button
+            disabled={!table.getCanPreviousPage()}
+            onClick={() => table.previousPage()}
+            size="sm"
+            variant="outline"
+          >
+            Previous
+          </Button>
+          <Button
+            disabled={!table.getCanNextPage()}
+            onClick={() => table.nextPage()}
+            size="sm"
+            variant="outline"
+          >
+            Next
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CustomTable;
---
+++ src/components/custom-table/types.ts
@@ -0,0 +1,20 @@
+import { ColumnDef, RowData, TableOptions } from '@tanstack/react-table';
+
+declare module '@tanstack/react-table' {
+  interface TableMeta<TData extends RowData> {
+    updateData: (rowIndex: number, columnId: string, value: unknown) => void;
+    deleteData: (rowIndex: number) => void;
+    duplicateData: (rowIndex: number) => void;
+    getData?: () => TData[];
+    isEditable?: boolean;
+    getSelectedData?: () => TData[];
+  }
+}
+
+export type CustomTableProps<T extends Record<string, unknown>> = {
+  columns: ColumnDef<T>[];
+  data: T[];
+  setData: React.Dispatch<React.SetStateAction<T[]>>;
+  options?: TableOptions<T>;
+  pageSize?: number;
+};
