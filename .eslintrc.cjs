b99dbc2d94f707c746c123382ebc46856fa76961
serifcolakel <serifcolakel0@gmail.com>
2023-11-11 21:16:05 +0300

    INFO (serif) : Add @assets to module path

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -96,6 +96,7 @@ module.exports = {
               ['^@routes(/.*|$)'],
               ['^@layouts(/.*|$)'],
               ['^@utils(/.*|$)'],
+              ['^@assets(/.*|$)'],
               ['^@helpers(/.*|$)'],
               ['^@hooks(/.*|$)'],
               ['^@providers(/.*|$)'],
