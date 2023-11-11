d3d09cc7bde2359281311376c8f4437a30e98a6a
serifcolakel <serifcolakel0@gmail.com>
2023-11-11 23:22:45 +0300

    INFO (serif) : Add CollectionPage component

---
+++ src/pages/collection/index.tsx
@@ -0,0 +1,7 @@
+export default function CollectionPage() {
+  return (
+    <div className="flex flex-col w-full h-full p-4 overflow-y-scroll">
+      <p>You can change your collection here.</p>
+    </div>
+  );
+}
