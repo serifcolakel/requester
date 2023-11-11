7579a6d42e8f9955aa8a2e874bca2e6f05abbc26
serifcolakel <serifcolakel0@gmail.com>
2023-11-11 23:21:58 +0300

    INFO (serif) : Add collection navbar to collection layout and environment navbar to environment layout

--- src/layouts/collection-layout/index.tsx
+++ src/layouts/collection-layout/index.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
 
+import CollectionNavbar from '@components/collection-navbar';
+
 export default function CollectionLayout() {
   return (
-    <div className="flex flex-row w-full h-screen divide-x">
-      124124124
+    <div className="flex flex-row w-full h-full divide-x">
+      <CollectionNavbar />
       <div className="w-full overflow-y-scroll no-scrollbar">
         <Outlet />
       </div>
--- src/layouts/environment-layout/index.tsx
+++ src/layouts/environment-layout/index.tsx
@@ -4,7 +4,7 @@ import EnvironmentNavbar from '@components/environment-navbar';
 
 export default function EnvironmentLayout() {
   return (
-    <div className="flex flex-row w-full h-screen divide-x">
+    <div className="flex flex-row w-full h-full divide-x">
       <EnvironmentNavbar />
       <div className="w-full overflow-y-scroll no-scrollbar">
         <Outlet />
