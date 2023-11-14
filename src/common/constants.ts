6c0b4c3a26c452972e4be22b6797bd895e23c1e3
serifcolakel <serifcolakel0@gmail.com>
2023-11-14 22:26:12 +0300

    Add METHOD_ENUMS constant and TMethod type.

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,3 +1,5 @@
+import { z } from 'zod';
+
 export const IPC_EVENTS = {
   CHANGE_BADGE_COUNT: 'CHANGE_BADGE_COUNT',
   CHANGE_TRAY: 'CHANGE_TRAY',
@@ -9,3 +11,7 @@ export const IPC_EVENTS = {
 export const LOCAL_STORAGE_KEYS = {
   TOKEN: import.meta.env.VITE_ACCESS_TOKEN_KEY as string,
 } as const;
+
+export const METHODS = ['GET', 'POST', 'PUT', 'DELETE'] as const;
+
+export const METHOD_ENUMS = z.enum(METHODS);
---
+++ src/common/types.ts
@@ -0,0 +1,3 @@
+import { METHOD_ENUMS } from './constants';
+
+export type TMethod = keyof typeof METHOD_ENUMS.Values;
